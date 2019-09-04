94c518316029f54910e0c196ae6e7fa29c83e50d
Cully Larson <cully.larson@gmail.com>
2019-09-04 11:40:41 -0700

    Removed a misleading comment that was probably copy/pasted and not removed.

--- esm/validateArrayNotEmpty.js
+++ esm/validateArrayNotEmpty.js
@@ -1,7 +1,6 @@
 import {curry} from '@cullylarson/f'
 import {messageObj, simpleValidationResult} from './index'
 
-// only works on strings and numbers
 export default curry((value, params) => {
     return !Array.isArray(value) || !value.length
         ? Promise.resolve(simpleValidationResult(messageObj('is-empty', 'Please provide a value.')))
