34fdb51391f080fae52eb1b746780e7941409592
Cully Larson <cully.larson@gmail.com>
2019-09-04 16:04:00 -0700

    Changed error messages for validateAtLeastOneMatches and validateOnlyOneMatches.

--- esm/validateAtLeastOneMatches.js
+++ esm/validateAtLeastOneMatches.js
@@ -5,7 +5,7 @@ import {messageObj, simpleValidationResult} from './index'
 // returns true for at least one value, the array is valid. Otherwise, not valid. Empty arrays are
 // always invalid.
 export default curry((f, value, params) => {
-    const getNotValid = () => Promise.resolve(simpleValidationResult(messageObj('no-match', 'Please provide a value.')))
+    const getNotValid = () => Promise.resolve(simpleValidationResult(messageObj('no-match', 'At least one matching value must be provided.')))
 
     if(!Array.isArray(value) || !value.length) return getNotValid()
 
