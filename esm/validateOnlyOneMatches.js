15d14c97c13c50a8a91b01ae1c2acfb1641dcc5c
Cully Larson <cully.larson@gmail.com>
2019-09-04 15:52:42 -0700

    Added validateOnlyOneMatches.

---
+++ esm/validateOnlyOneMatches.js
@@ -0,0 +1,17 @@
+import {curry} from '@cullylarson/f'
+import {messageObj, simpleValidationResult} from './index'
+
+// Validates an array. The provided function is called for each value in the array. If the function
+// returns true for only one value, the array is valid. Otherwise, not valid. Empty arrays are
+// always invalid.
+export default curry((f, value, params) => {
+    const getNotValid = () => Promise.resolve(simpleValidationResult(messageObj('no-match', 'Please provide a value.')))
+
+    if(!Array.isArray(value) || !value.length) return getNotValid()
+
+    const matches = value.filter(f)
+
+    return matches.length === 1
+        ? Promise.resolve(simpleValidationResult())
+        : getNotValid()
+})
--- test/validateOnlyOneMatches.test.js
+++ test/validateOnlyOneMatches.test.js
@@ -1,23 +1,23 @@
-import validateAtLeastOneMatches from '../esm/validateAtLeastOneMatches'
+import validateOnlyOneMatches from '../esm/validateOnlyOneMatches'
 
 test('Array with one value matching is valid.', () => {
-    return validateAtLeastOneMatches(x => x === 2, [1, 2, 3], {})
+    return validateOnlyOneMatches(x => x === 2, [1, 2, 3], {})
         .then(x => {
             expect(x.isValid).toBe(true)
             expect(x.messages.length).toBe(0)
         })
 })
 
-test('Array with two values matching is valid.', () => {
-    return validateAtLeastOneMatches(x => x === 2, [1, 2, 3, 2, 5], {})
+test('Array with two matching values is not valid.', () => {
+    return validateOnlyOneMatches(x => x === 2, [1, 2, 3, 2, 5], {})
         .then(x => {
-            expect(x.isValid).toBe(true)
-            expect(x.messages.length).toBe(0)
+            expect(x.isValid).toBe(false)
+            expect(x.messages.filter(x => x.code === 'no-match').length).toBe(1)
         })
 })
 
 test('Empty array is not valid.', () => {
-    return validateAtLeastOneMatches(x => x === 2, [], {})
+    return validateOnlyOneMatches(x => x === 2, [], {})
         .then(x => {
             expect(x.isValid).toBe(false)
             expect(x.messages.filter(x => x.code === 'no-match').length).toBe(1)
@@ -25,7 +25,7 @@ test('Empty array is not valid.', () => {
 })
 
 test('Array with no matching values is not valid.', () => {
-    return validateAtLeastOneMatches(x => x === 2, [3, 4, 5], {})
+    return validateOnlyOneMatches(x => x === 2, [3, 4, 5], {})
         .then(x => {
             expect(x.isValid).toBe(false)
             expect(x.messages.filter(x => x.code === 'no-match').length).toBe(1)
