a7684701dcb4936a92926b8e36cf237d598e3bf4
Cully Larson <cully.larson@gmail.com>
2020-06-19 12:47:13 -0700

    Added the validateDollars validator.

---
+++ esm/validateDollars.js
@@ -0,0 +1,34 @@
+import {curry, isFloat} from '@cullylarson/f'
+import {messageObj, simpleValidationResult} from './index'
+
+const digitsAfterDecimal = x => {
+    const [, after] = x.toString().split('.')
+
+    if(after === undefined) return 0
+
+    return after.split('').length
+}
+
+export default curry(({allowZero = true, allowNegative = true}, value, params) => {
+    const notMoneyResult = simpleValidationResult(messageObj('not-dollars', 'Please provide a validate dollar amount.'))
+
+    if(!isFloat(value)) {
+        return Promise.resolve(notMoneyResult)
+    }
+
+    const floatValue = parseFloat(value)
+
+    if(!allowZero && floatValue === 0) {
+        return Promise.resolve(simpleValidationResult(messageObj('is-zero', 'A zero amount is not allowed.')))
+    }
+
+    if(!allowNegative && floatValue < 0) {
+        return Promise.resolve(simpleValidationResult(messageObj('is-negative', 'A negative amount is not allowed.')))
+    }
+
+    if(digitsAfterDecimal(floatValue) > 2) {
+        return Promise.resolve(notMoneyResult)
+    }
+
+    return Promise.resolve(simpleValidationResult())
+})
---
+++ test/validateDollars.test.js
@@ -0,0 +1,116 @@
+import {map} from '@cullylarson/f'
+import validateDollars from '../esm/validateDollars'
+
+test('A float dollar amount is valid.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: true}, 1, {}),
+        validateDollars({allowZero: true, allowNegative: true}, 1000.13, {}),
+        validateDollars({allowZero: true, allowNegative: true}, 0.03, {}),
+        validateDollars({allowZero: true, allowNegative: true}, -1, {}),
+        validateDollars({allowZero: true, allowNegative: true}, -1000.23, {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(true)
+            expect(x.messages.length).toBe(0)
+        }))
+})
+
+test('A float string dollar amount is valid.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: true}, '1', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '1000.34', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '0.20', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '-1', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '-1000.13', {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(true)
+            expect(x.messages.length).toBe(0)
+        }))
+})
+
+test('A float with too many decimal places is not valid.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: true}, 1000.124, {}),
+        validateDollars({allowZero: true, allowNegative: true}, 0.034, {}),
+        validateDollars({allowZero: true, allowNegative: true}, '34.788', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '.777', {}),
+        validateDollars({allowZero: true, allowNegative: true}, -1000.23994, {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'not-dollars').length).toBe(1)
+        }))
+})
+
+test('Zero is not valid when not allowed.', () => {
+    return Promise.all([
+        validateDollars({allowZero: false, allowNegative: true}, 0, {}),
+        validateDollars({allowZero: false, allowNegative: true}, '0', {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'is-zero').length).toBe(1)
+        }))
+})
+
+test('Negative is invalid when not allowed.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: false}, -1, {}),
+        validateDollars({allowZero: true, allowNegative: false}, '-1', {}),
+        validateDollars({allowZero: true, allowNegative: false}, -78.02, {}),
+        validateDollars({allowZero: true, allowNegative: false}, '-88.88', {}),
+        validateDollars({allowZero: true, allowNegative: false}, -0.01, {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'is-negative').length).toBe(1)
+        }))
+})
+
+test('A non-float string is not valid.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: true}, 'one', {}),
+        validateDollars({allowZero: true, allowNegative: true}, 'foo', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '', {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'not-dollars').length).toBe(1)
+        }))
+})
+
+test('A string starting with a number is not valid.', () => {
+    return Promise.all([
+        validateDollars({allowZero: true, allowNegative: true}, '1 one', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '3 foo', {}),
+        validateDollars({allowZero: true, allowNegative: true}, '4asdf', {}),
+    ])
+        .then(map(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'not-dollars').length).toBe(1)
+        }))
+})
+
+test('An array is not valid.', () => {
+    validateDollars({allowZero: true, allowNegative: true}, [], {})
+        .then(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'not-dollars').length).toBe(1)
+        })
+})
+
+test('An object is not valid.', () => {
+    validateDollars({allowZero: true, allowNegative: true}, {}, {})
+        .then(x => {
+            expect(x.isValid).toBe(false)
+            expect(x.messages.length).toBe(1)
+            expect(x.messages.filter(x => x.code === 'not-dollars').length).toBe(1)
+        })
+})
